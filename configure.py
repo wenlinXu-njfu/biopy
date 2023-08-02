702edf80fd680d2573e4f29b101bca69e45ab0e5
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2023-08-02 10:36:21 +0800

    first commit

--- configure.py
+++ configure.py
@@ -1,13 +1,11 @@
 from os import system, listdir, remove
-from os.path import dirname, isfile
+from os.path import abspath, isfile
 from shutil import rmtree
 
-path = dirname(__file__)
+path = abspath(__file__).replace(__file__, '')
 system(command=f"sed -i 's/\r//' {path}/bin/*")
 system(command=f"chmod u+x {path}/bin/*")
 system(command=f"python {path}/bin/batch_rename.py -d {path}/bin -old .py")
 for file in listdir(path):
     if isfile(path + f'/{file}'):
         remove(path + f'/{file}')
-    elif file == 'molbreeding' or file == '.idea':
-        rmtree(f'{path}/{file}')
