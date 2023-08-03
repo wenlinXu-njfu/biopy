f2da9a11af20182a0a5bbb76ad9f6c25d01274f6
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2023-08-03 21:30:34 +0800

    update

--- file_format_conversion_lib/fasta_conversion.py
+++ file_format_conversion_lib/fasta_conversion.py
@@ -8,10 +8,9 @@ E-mail: wenlinxu.njfu@outlook.com
 """
 import click
 from Biolib.fasta import Fasta
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
-def main(in_file, char_num, out_file):
+def main(in_file, char_num: int, out_file):
     fa_file_obj = Fasta(in_file)
     content = []
     if char_num == 0:
@@ -32,12 +31,12 @@ def main(in_file, char_num, out_file):
             o.write(''.join(content))
 
 
-@click.command(context_settings=CONTEXT_SETTINGS)
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('-i', '--fasta_file', 'fasta_file', help='Input FASTA file.')
-@click.option('-n', '--char_num', 'char_num', type=int, default=60,
-              help='Specify how many character show in per line, 0 presents one line show per sequence. {default: 60}')
+@click.option('-n', '--char_num', 'char_num', type=int, default=60, show_default=True,
+              help='Specify how many character show in per line, 0 presents one line show per sequence.')
 @click.option('-o', '--output_file', 'outfile',
-              help='[optional] Output file, if not specified, print results to terminal as stdout.')
+              help='Output file, if not specified, print results to terminal as stdout.')
 def run(fasta_file, char_num, outfile):
     """Make each sequence to be displayed on a single line or in multiple lines."""
     main(fasta_file, char_num, outfile)
