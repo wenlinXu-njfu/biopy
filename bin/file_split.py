575dfac3b4e41e1ada45581cb8f0719342ed8841
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2023-08-06 09:46:01 +0800

    update

--- bin/file_split.py
+++ bin/file_split.py
@@ -1,35 +1,43 @@
 #!/usr/bin/env python
 """
 File: file_split.py
-Description: Divide a large file into several smaller files
+Description: Divide a large file into several smaller files.
 Date: 2022/1/23
 Author: xuwenlin
 E-mail: wenlinxu.njfu@outlook.com
 """
 from os import mkdir
 from os.path import exists
+from typing import Union
+from _io import TextIOWrapper
 import click
+from click._compat import _NonClosingTextIOWrapper
 from Biolib.show_info import Displayer
 
 
-def file_split(in_file: str,
-               sub_file_line_num: int,
-               out_dir: str,
-               header: bool = True,
-               header_line_num: int = 1):
-    if not exists(out_dir):
-        mkdir(out_dir)
-    line_num = sum(1 for _ in open(in_file))
-    line_count = 0
-    subfile_num = 1
+def main(input_file: Union[TextIOWrapper, list],
+         sub_file_line_num: int,
+         output_dir: str,
+         header: bool,
+         header_line_num: int):
+    if not exists(output_dir):
+        mkdir(output_dir)
+    line_count, subfile_num = 0, 1
+    line_num = sum(1 for _ in input_file)
     header_content = content = ''
-    out_prefix = in_file.split('/')[-1]
-    for line in open(in_file):
+    # Read content from a file.
+    if isinstance(input_file, TextIOWrapper):
+        output_prefix = f"{input_file.name.split('/')[-1]}."
+        input_file = open(input_file.name)
+    # Read content from stdout.
+    else:
+        output_prefix = ''
+    for line in input_file:
         line_count += 1
         if header:
             if line_count == line_num:
                 content += line
-                with open(f"{out_dir}/{out_prefix}.part{subfile_num}", 'w') as o:
+                with open(f"{output_dir}/{output_prefix}part{subfile_num}", 'w') as o:
                     o.write(header_content + content)
             elif line_count <= header_line_num:
                 header_content += line
@@ -38,39 +46,41 @@ def file_split(in_file: str,
                     content += line
                 else:
                     content += line
-                    with open(f"{out_dir}/{out_prefix}.part{subfile_num}", 'w') as o:
+                    with open(f"{output_dir}/{output_prefix}part{subfile_num}", 'w') as o:
                         o.write(header_content + content)
                     content = ''
                     subfile_num += 1
         else:
             if line_count == line_num:
                 content += line
-                with open(f"{out_dir}/{out_prefix}.part{subfile_num}", 'w') as o:
+                with open(f"{output_dir}/{output_prefix}part{subfile_num}", 'w') as o:
                     o.write(content)
             elif line_count % sub_file_line_num != 0:
                 content += line
             else:
                 content += line
-                with open(f"{out_dir}/{out_prefix}.part{subfile_num}", 'w') as o:
+                with open(f"{output_dir}/{output_prefix}part{subfile_num}", 'w') as o:
                     o.write(content)
                 content = ''
                 subfile_num += 1
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('-i', '--input_file', 'input_file', help='Input file.')
+@click.option('-i', '--input_file', 'input_file', type=click.File('r'), help='Input file.')
 @click.option('-n', '--line_num', 'line_num', type=int, help='The line num of each sub file, not including header.')
 @click.option('-H', '--header', is_flag=True, flag_value=True,
               help='If specified header, each sub file will contain header content.')
 @click.option('-N', '--header_num', 'header_num', type=int, default=1, show_default=True,
-              help='If header specified, specify the number of header line.')
+              help='[optional] If header specified, specify the number of header line.')
 @click.option('-o', '--output_dir', 'output_dir',
               help='Output directory, if not exists, it will be created automatically.')
 @click.option('-V', '--version', 'version', help='Show author and version information.',
               is_flag=True, is_eager=True, expose_value=False, callback=Displayer(__file__.split('/')[-1]).version_info)
 def run(input_file, line_num, header, header_num, output_dir):
     """Divide a large file into several smaller files."""
-    file_split(input_file, line_num, output_dir, header, header_num)
+    if isinstance(input_file, _NonClosingTextIOWrapper):
+        input_file = click.open_file('-').readlines()
+    main(input_file, line_num, output_dir, header, header_num)
 
 
 if __name__ == '__main__':
--- bin/id_index_seq.py
+++ bin/id_index_seq.py
@@ -6,36 +6,37 @@ Date: 2022/3/25
 Author: xuwenlin
 E-mail: wenlinxu.njfu@outlook.com
 """
+from typing import Tuple
+from _io import TextIOWrapper
 import click
 from Biolib.fasta import Fasta
 from Biolib.show_info import Displayer
 
 
-def main(fasta_files,
+def main(fasta_files: Tuple[TextIOWrapper],
          parse_seqids: bool,
-         id_file,
+         id_file: TextIOWrapper,
          match: bool,
-         log,
-         out_file):
+         log: str,
+         out_file: str):
     content = []
-    id_list = list(set(line.strip() for line in id_file))
-    id_list_index = list(range(len(id_list)))  # Create a subscript for each ID
+    raw_id_set = set(line.strip() for line in id_file)
+    have_found_id_set = set()
     for fasta_file in fasta_files:
         fasta_file = str(fasta_file)
         file_name = fasta_file.split('/')[-1]
         for seq_obj in Fasta(fasta_file).parse(parse_seqids):
-            if match and seq_obj.id in id_list:
-                del id_list[id_list.index(seq_obj.id)]
+            if match and seq_obj.id in raw_id_set:
+                have_found_id_set.add(seq_obj.id)
                 if out_file:
                     content.append(f">{seq_obj.id} from={file_name}\n{seq_obj.seq}\n")
                 else:
                     seq_obj.id = f'{seq_obj.id} from={file_name}'
                     print(seq_obj)
             elif not match:
-                for _id in id_list:
+                for _id in raw_id_set:
                     if seq_obj.id in _id or _id in seq_obj.id:
-                        index = id_list.index(_id)
-                        id_list_index[index] = True  # Mark sequence that has been found
+                        have_found_id_set.add(_id)
                         if out_file:
                             content.append(f">{seq_obj.id} from={file_name}\n{seq_obj.seq}\n")
                         else:
@@ -44,24 +45,18 @@ def main(fasta_files,
     if content:
         with open(out_file, 'w') as o:
             o.write(''.join(content))
-    # report sequence that not match
-    if match and id_list:
-        msg = ' not found\n'.join(id_list) + ' not found\n'
+    # report sequence that not found.
+    not_found_id = list(raw_id_set - have_found_id_set)
+    not_found_id.sort()
+    if not_found_id:
+        msg = ' not found\n'.join(not_found_id) + ' not found\n'
         click.echo(f'\033[33m{msg}\033[0m', err=True, file=open(log, 'a')) if log else \
             click.echo(f'\033[33m{msg}\033[0m', err=True)
-    # report sequence that not contain
-    if not match:
-        not_found = [i for i in id_list_index if i is not True]
-        not_found = [id_list[i] for i in not_found]
-        if not_found:
-            msg = ' not found\n'.join(not_found) + ' not found\n'
-            click.echo(f'\033[33m{msg}\033[0m', err=True, file=open(log, 'a')) if log else \
-                click.echo(f'\033[33m{msg}\033[0m', err=True)
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.argument('fasta_files', nargs=-1)
-@click.option('-P', '--parse_seqids', 'parse_seqids', is_flag=True, flag_value=True, help='Parse sequence id.')
+@click.option('-P', '--parse_seqids', 'parse_seqids', is_flag=True, flag_value=True, help='Parse sequence id in FASTA file.')
 @click.option('-I', '--id_file', 'id_file', type=click.File('r'), help='Input id TEXT file (one id per line).')
 @click.option('--match/--contain', default=True, show_default=True,
               help='Whether the id supplied should exactly match the ID of the sequence.')
