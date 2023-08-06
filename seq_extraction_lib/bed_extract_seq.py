6ef7d1bfbc473731618eb7c231f7c7c44d0fdbc1
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2023-08-06 21:03:11 +0800

    update

--- seq_extraction_lib/bed_extract_seq.py
+++ seq_extraction_lib/bed_extract_seq.py
@@ -1,61 +1,58 @@
 #!/usr/bin/env python
 """
 File: bed_extract_seq.py
-Description: Extract sequences from BED file
+Description: Extract sequences from BED file.
 Date: 2022/3/21
 Author: xuwenlin
 E-mail: wenlinxu.njfu@outlook.com
 """
+from _io import TextIOWrapper
 import click
 from Biolib.bed import Bed
 from Biolib.show_info import Displayer
 
 
-def main(bed_file: str,
-         fa_file: str,
+def main(bed_file: TextIOWrapper,
+         fa_file: TextIOWrapper,
          use_id: bool,
          up: int,
          down: int,
          both: int,
          extension: bool,
-         out_file: str = None):
-    bed_file_obj = Bed(bed_file)
-    nucl_obj_generator = bed_file_obj.bed_extract_seq(fa_file, use_id, up, down, both, extension)
-    content = ''
-    for nucl_obj in nucl_obj_generator:
-        if out_file:
-            content += f">{nucl_obj.id}\n{nucl_obj.seq}\n"
-        else:
-            print(f">{nucl_obj.id}\n{nucl_obj.seq}")
+         out_file: TextIOWrapper = None):
+    content = []
+    for nucl_obj in Bed(bed_file).bed_extract_seq(fa_file, use_id, up, down, both, extension):
+        content.append(f">{nucl_obj.id}\n{nucl_obj.seq}\n") if out_file else print(f">{nucl_obj.id}\n{nucl_obj.seq}")
     if out_file:
-        with open(out_file, 'w') as o:
-            o.write(content)
+        with out_file as o:
+            o.write(''.join(content))
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('-i', '--bed_file', 'bed_file',
+@click.option('-i', '--bed_file', 'bed_file', type=click.File('r'),
               help='Input BED file.\n(chr_num\\tstart\\tend\\tframe\\tname\\tstrand\\tetc)')
-@click.option('-f', '--ref_fasta', 'ref_fasta_file', help='Input reference sequence FASTA file.')
-@click.option('-up', '--upstream', 'upstream', type=int, default=0, show_default=True,
+@click.option('-r', '--ref_fasta', 'ref_fasta_file', type=click.File('r'), help='Input reference sequence FASTA file.')
+@click.option('-u', '--upstream', 'upstream', type=int, default=0, show_default=True,
               help='Make sequence in bed file to extent upstream the specified length.')
-@click.option('-down', '--downstream', 'downstream', type=int, default=0, show_default=True,
+@click.option('-d', '--downstream', 'downstream', type=int, default=0, show_default=True,
               help='Make sequence in bed file to extent downstream the specified length.')
-@click.option('-b', '--both', 'both', type=int, default=0, show_default=True,
-              help='''Make sequence in bed file to extent both end the specified length. 
-              If "up" "down" and "both" are specified, by default, only both is valid.''')
-@click.option('--use_id', is_flag=True, flag_value=True,
-              help='''Use the fourth column content in the BED file as sequence ID. 
-              Otherwise, "Chr_num:start-end(strand)" by default.''')
+@click.option('-b', '--both_end', 'both_end', type=int, default=0, show_default=True,
+              help='Make sequence in bed file to extent both end the specified length. '
+                   'If "-u --upstream", "-d --downstream", and "-b --both_end" are specified, '
+                   'by default, only "--both_end" is valid.')
+@click.option('-U', '--use_id', 'use_id', is_flag=True, flag_value=True,
+              help='Use the fourth column content in the BED file as sequence ID. '
+                   'Otherwise, "Chr_num:start-end(strand)" by default.')
 @click.option('--extension/--non_extension', type=bool, default=True, show_default=True,
-              help='''If "up" "down" or "both" is specified, specify whether extension 
-              (including sequence self in BED file).''')
-@click.option('-o', '--output_file', 'output_file',
+              help='If "-u --upstream", "-d --downstream" or "-b --both_end" is specified, '
+                   'specify whether extension (including sequence self in BED file).')
+@click.option('-o', '--output_file', 'output_file', type=click.File('w'),
               help='Output FASTA file, if not specified, print results to terminal as stdout.')
 @click.option('-V', '--version', 'version', help='Show author and version information.',
               is_flag=True, is_eager=True, expose_value=False, callback=Displayer(__file__.split('/')[-1]).version_info)
-def run(bed_file, ref_fasta_file, use_id, upstream, downstream, both, extension, output_file):
+def run(bed_file, ref_fasta_file, use_id, upstream, downstream, both_end, extension, output_file):
     """Extract sequences from BED file."""
-    main(bed_file, ref_fasta_file, use_id, upstream, downstream, both, extension, output_file)
+    main(bed_file, ref_fasta_file, use_id, upstream, downstream, both_end, extension, output_file)
 
 
 if __name__ == '__main__':
--- seq_extraction_lib/gff_extract_seq.py
+++ seq_extraction_lib/gff_extract_seq.py
@@ -1,44 +1,49 @@
 #!/usr/bin/env python
 """
 File: gff_extract_seq.py
-Description: Extract sequences from GFF file
+Description: Extract sequences from GFF file.
 Date: 2022/3/24
 Author: xuwenlin
 E-mail: wenlinxu.njfu@outlook.com
 """
+from _io import TextIOWrapper
 import click
 from Biolib.gff import Gff
 from Biolib.show_info import Displayer
 
 
-def main(gff_file, fa_file, feature_type, id_file, out_file):
-    content = ''
+def main(gff_file: TextIOWrapper,
+         fa_file: TextIOWrapper,
+         feature_type: str,
+         id_file: TextIOWrapper,
+         out_file: TextIOWrapper):
+    content = []
     if id_file:
-        id_list = [i.strip() for i in open(id_file).readlines() if i.strip()]
+        id_list = set(i.strip() for i in id_file.readlines() if i.strip())
         for nucl_obj in Gff(gff_file).gff_extract_seq(fa_file, feature_type, id_list):
             if out_file:
-                content += f">{nucl_obj.id}\n{nucl_obj.seq}\n"
+                content.append(f">{nucl_obj.id}\n{nucl_obj.seq}\n")
             else:
                 print(nucl_obj)
     else:
         for nucl_obj in Gff(gff_file).gff_extract_seq(fa_file, feature_type):
             if out_file:
-                content += f">{nucl_obj.id}\n{nucl_obj.seq}\n"
+                content.append(f">{nucl_obj.id}\n{nucl_obj.seq}\n")
             else:
                 print(nucl_obj)
     if content:
-        with open(out_file, 'w') as o:
-            o.write(content)
+        with out_file as o:
+            o.write(''.join(content))
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('-g', '--gff_file', 'gff_file', help='Input GFF file.')
-@click.option('-f', '--ref_fasta', 'ref_fasta_file', help='Input reference sequence FASTA file.')
+@click.option('-g', '--gff_file', 'gff_file', type=click.File('r'), help='Input GFF file.')
+@click.option('-r', '--ref_fasta', 'ref_fasta_file', type=click.File('r'), help='Input reference sequence FASTA file.')
 @click.option('-t', '--feature_type', 'feature_type', help='Specify feature type. (eg: gene, mRNA, etc)')
-@click.option('-d', '--id_file', 'id_file',
+@click.option('-d', '--id_file', 'id_file', type=click.File('r'),
               help='[optional] Provides an ID file (one id per line) that extracts sequences from the GFF file that '
                    '\033[1mmatch\033[0m the IDs in the ID file.')
-@click.option('-o', '--output_file', 'output_file',
+@click.option('-o', '--output_file', 'output_file', type=click.File('w'),
               help='Output file, if not specified, print results to terminal as stdout.')
 @click.option('-V', '--version', 'version', help='Show author and version information.',
               is_flag=True, is_eager=True, expose_value=False, callback=Displayer(__file__.split('/')[-1]).version_info)
