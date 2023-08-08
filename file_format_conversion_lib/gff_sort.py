c94a1ea52a768897c9f2e8905c7712ac110a66a2
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2023-08-08 13:46:02 +0800

    update

--- file_format_conversion_lib/gff_sort.py
+++ file_format_conversion_lib/gff_sort.py
@@ -6,24 +6,25 @@ Date: 2022/3/31
 Author: xuwenlin
 E-mail: wenlinxu.njfu@outlook.com
 """
+from _io import TextIOWrapper
 import click
 from Biolib.gff import Gff
 from Biolib.show_info import Displayer
 
 
-def main(gff_file, out_file):
+def main(gff_file: TextIOWrapper, out_file: TextIOWrapper = None):
     gff_file_obj = Gff(gff_file)
     content = gff_file_obj.gff_sort()
     if out_file:
-        with open(out_file, 'w') as o:
+        with out_file as o:
             o.write(content)
     else:
         print(content)
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('-i', '--gff_file', 'gff_file', help='Input unsorted GFF file.')
-@click.option('-o', '--output_file', 'output_file',
+@click.option('-i', '--gff_file', 'gff_file', type=click.File('r'), help='Input unsorted GFF file.')
+@click.option('-o', '--output_file', 'output_file', type=click.File('w'),
               help='Output sorted GFF file, if not specified, print results to terminal as stdout.')
 @click.option('-V', '--version', 'version', help='Show author and version information.',
               is_flag=True, is_eager=True, expose_value=False, callback=Displayer(__file__.split('/')[-1]).version_info)
