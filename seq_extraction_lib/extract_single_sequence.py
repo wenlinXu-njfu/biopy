c70a81da289f0577b7b81e697153a729b7eb6a39
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2023-08-11 19:36:11 +0800

    update

--- seq_extraction_lib/extract_single_sequence.py
+++ seq_extraction_lib/extract_single_sequence.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 File: extract_single_sequence.py
-Description: Extract one sub-sequence from reference sequence file
+Description: Extract one sub-sequence from reference sequence file.
 Date: 2022/5/3
 Author: xuwenlin
 E-mail: wenlinxu.njfu@outlook.com
@@ -29,13 +29,14 @@ def main(fasta_file: TextIOWrapper,
                     o.write(f">{sub_seq.id}\n{sub_seq.seq}\n")
             else:
                 print(sub_seq)
+            break
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('-r', '--ref_fasta', 'ref_fasta_file', type=click.File('r'), help='Input reference sequence file. (format: FASTA)')
 @click.option('-c', '--chr_name', 'chr_name', help='Chromosome name. (eg:Chr01)')
-@click.option('-s', '--start_site', 'start_site', type=int, help='Start site on chromosome.')
-@click.option('-e', '--end_site', 'end_site', type=int, help='End site on chromosome.')
+@click.option('-s', '--start_site', 'start_site', type=int, help='Start site on chromosome. (based on 1)')
+@click.option('-e', '--end_site', 'end_site', type=int, help='End site on chromosome. (based on 1)')
 @click.option('-S', '--strand', 'strand', type=click.Choice(['+', '-']), help='Direction of the chain.')
 @click.option('-o', '--output_file', 'output_file', type=click.File('w'),
               help='Output file, if not specified, print result to terminal as stdout.')
