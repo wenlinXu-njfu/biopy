91c4871a34bec54452084a1d1da98175d7943131
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2023-08-13 15:21:14 +0800

    update

--- Biolib/timer.py
+++ Biolib/timer.py
@@ -10,18 +10,22 @@ from datetime import datetime
 
 
 class Timer:
-    def __init__(self, message: str = None):
-        self.message = message
+    def __init__(self, start_message: str = None, command_content: str = None):
+        self.start_message = start_message
+        self.command_content = command_content
 
     def __call__(self, function):
         def wrapper(*args, **kwargs):
             start_time = datetime.now().replace(microsecond=0)
-            if self.message:
-                click.echo(f"[{datetime.now().replace(microsecond=0)}] {self.message}", err=True)
+            if self.start_message:
+                click.echo(f"[{datetime.now().replace(microsecond=0)}] {self.start_message}", err=True)
             else:
                 msg = function.__name__.replace('_', ' ')
                 click.echo(f"[{datetime.now().replace(microsecond=0)}] Start run {msg}.", err=True)
+            if self.command_content:
+                click.echo(f"[{datetime.now().replace(microsecond=0)}] {self.command_content}", err=True)
+            value = function(*args, **kwargs)
             end_time = datetime.now().replace(microsecond=0)
             click.echo(f'[{datetime.now().replace(microsecond=0)}] Finish in {end_time - start_time}.', err=True)
-            return function(*args, **kwargs)
+            return value
         return wrapper
