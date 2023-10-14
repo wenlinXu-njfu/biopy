c1736077ca54b3581fcef55ab8062f48eb2b8224
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2023-10-14 21:17:22 +0800

    update

--- file_format_conversion_lib/__init__.py
+++ file_format_conversion_lib/__init__.py
@@ -4,3 +4,4 @@ from file_format_conversion_lib.gff2bed import run as gff2bed
 from file_format_conversion_lib.gff2gtf import run as gff2gtf
 from file_format_conversion_lib.gff_sort import run as gff_sort
 from file_format_conversion_lib.gtf2bed import run as gtf2bed
+from file_format_conversion_lib.fa2tab import run as fa2tab
