d01f9ecb363afaaf9a3e1802d230e72ec9a113f6
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2023-11-04 11:35:14 +0800

    update

--- plot_lib/plot_cluster_heatmap.py
+++ plot_lib/plot_cluster_heatmap.py
@@ -1,42 +1,62 @@
 #!/usr/bin/env python
 """
 File: plot_cluster_heatmap.py
-Description: Plot gene expression cluster heatmap
+Description: Plot gene expression cluster heatmap.
 CreateDate: 2022/4/27
 Author: xuwenlin
 E-mail: wenlinxu.njfu@outlook.com
 """
 import click
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from pybioinformatic import Displayer
+from numpy import log2
+from matplotlib.pyplot import rcParams, savefig
+from seaborn import clustermap
+from pybioinformatic import read_in_gene_expression_as_dataframe, Displayer
 displayer = Displayer(__file__.split('/')[-1], version='0.1.0')
 
 
-def main(gene_exp_file: str, color_map: str, out_file: str):
-    plt.rcParams['pdf.fonttype'] = 42
-    plt.rcParams['font.family'] = 'Arial'
-    df = pd.read_table(gene_exp_file, index_col=0)
-    sns.clustermap(df, cmap=color_map, standard_scale=0)
-    plt.savefig(out_file)
+def main(gene_exp_file: str,
+         color_map: str,
+         row_cluster: bool,
+         col_cluster: bool,
+         out_file: str):
+    rcParams['pdf.fonttype'] = 42
+    rcParams['font.family'] = 'Arial'
+    df = read_in_gene_expression_as_dataframe(gene_exp_file)
+    df = log2(df + 1)
+    df.index.name = None
+    if len(df) <= 150:
+        tree_kws = {'linewidth': 0.3}
+    elif 150 < len(df) <= 500:
+        tree_kws = {'linewidth': 0.1}
+    else:
+        tree_kws = {'linewidth': 0.01, 'alpha': 0.3}
+    clustermap(df, cmap=color_map, yticklabels=False, tree_kws=tree_kws,
+               row_cluster=row_cluster, col_cluster=col_cluster,
+               cbar_pos=(1.02, 0.5, 0.025, 0.25))
+    savefig(out_file, bbox_inches='tight')
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('-i', '--gene_exp', 'gene_exp_file',
-              metavar='<exp file>', required=True,
-              help='Input gene expression file. (TAB split)')
+              metavar='<text file>', required=True,
+              help='Input gene expression file (support format: txt, csv, xls, and xlsx).')
 @click.option('-c', '--color_map', 'color_map',
-              metavar='<str>', default='vlag', show_default=True,
-              help='Color map.')
+              metavar='<str>', default='PiYG', show_default=True,
+              help='Color map (eg. PiYG, vlag, OrRd, YlOrRd, Spectral).')
+@click.option('--row_cluster', 'row_cluster',
+              type=bool, is_flag=True, flag_value=True,
+              help='If specified, cluster the rows.')
+@click.option('--col_cluster', 'col_cluster',
+              type=bool, is_flag=True, flag_value=True,
+              help='If specified, cluster the columns.')
 @click.option('-o', '--output_file', 'outfile',
               metavar='<figure file>', default='cluster_heatmap.pdf', show_default=True,
               help='Output file.')
 @click.option('-V', '--version', 'version', help='Show author and version information.',
               is_flag=True, is_eager=True, expose_value=False, callback=displayer.version_info)
-def run(gene_exp_file, color_map, outfile):
+def run(gene_exp_file, color_map, row_cluster, col_cluster, outfile):
     """Plot gene expression cluster heatmap."""
-    main(gene_exp_file, color_map, outfile)
+    main(gene_exp_file, color_map, row_cluster, col_cluster, outfile)
 
 
 if __name__ == '__main__':
--- plot_lib/plot_heatmap.py
+++ plot_lib/plot_heatmap.py
@@ -6,36 +6,39 @@ CreateDate: 2022/4/15
 Author: xuwenlin
 E-mail: wenlinxu.njfu@outlook.com
 """
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+from numpy import log2
+from matplotlib.pyplot import rcParams, savefig
+from seaborn import heatmap
 import click
 from pybioinformatic import read_in_gene_expression_as_dataframe, Displayer
 displayer = Displayer(__file__.split('/')[-1], version='0.1.0')
 
 
-def main(gene_exp_file: str, out_file: str):
-    plt.rcParams['pdf.fonttype'] = 42
-    plt.rcParams['font.family'] = 'Arial'
+def main(gene_exp_file: str, color_map: str, out_file: str):
+    rcParams['pdf.fonttype'] = 42
+    rcParams['font.family'] = 'Arial'
     df = read_in_gene_expression_as_dataframe(gene_exp_file)
-    df = df + 1
-    df = np.log2(df)
-    sns.clustermap(df, cmap='Pastel1', yticklabels=False, col_cluster=False, z_score=0, figsize=(10, 20))
-    plt.savefig(out_file)
+    df = log2(df + 1)
+    df.index.name = None
+    heatmap(df, cmap=color_map, yticklabels=False)
+    savefig(out_file)
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('-i', '--gene_exp', 'gene_exp_file',
               metavar='<exp file>', required=True,
               help='Input gene expression file. (support format: txt, xls, xlsx, and csv)')
+@click.option('-c', '--color_map', 'color_map',
+              metavar='<str>', default='PiYG', show_default=True,
+              help='Color map (eg. PiYG, vlag, OrRd, YlOrRd, Spectral).')
 @click.option('-o', '--output_file', 'output_file',
               metavar='<figure file>', default='heatmap.pdf', show_default=True,
               help='Output file.')
 @click.option('-V', '--version', 'version', help='Show author and version information.',
               is_flag=True, is_eager=True, expose_value=False, callback=displayer.version_info)
-def run(gene_exp_file, output_file):
+def run(gene_exp_file, color_map, output_file):
     """Plot gene expression heatmap."""
-    main(gene_exp_file, output_file)
+    main(gene_exp_file, color_map, output_file)
 
 
 if __name__ == '__main__':
