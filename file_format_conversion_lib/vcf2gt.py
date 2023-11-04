9ca154b99204ff3427550ea766a2bc046a480702
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2023-11-04 18:25:58 +0800

    update vcf module

--- file_format_conversion_lib/vcf2gt.py
+++ file_format_conversion_lib/vcf2gt.py
@@ -14,29 +14,25 @@ displayer = Displayer(__file__.split('/')[-1], version='0.1.0')
 
 
 def main(vcf_file: Union[str, TextIOWrapper],
-         num_processing: int,
          output_file: Union[str, TextIOWrapper]):
     with VCF(vcf_file) as vcf:
-        gt = vcf.to_genotype(num_processing)
-        gt.to_csv(output_file, sep='\t')
+        for line in vcf.to_genotype():
+            click.echo(line, output_file)
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('-i', '--vcf_file', 'vcf_file',
               metavar='<vcf file>', type=click.File('r'), required=True,
               help='Input VCF file.')
-@click.option('-n', '--num_processing', 'num_processing',
-              metavar='<int>', type=int, default=1, show_default=True,
-              help='Number of processing.')
 @click.option('-o', '--output_file', 'output_file',
-              metavar='<file>', type=click.File('w'), default='GT.xls', show_default=True,
-              help='Output file.')
+              metavar='<file>', type=click.File('w'),
+              help='Output file, print results to terminal by default.')
 @click.option('-V', '--version', 'version', help='Show author and version information.',
               is_flag=True, is_eager=True, expose_value=False, callback=displayer.version_info)
 @Timer('Converting the vcf file to gt format.')
-def run(vcf_file, num_processing, output_file):
+def run(vcf_file, output_file):
     """Convert the file format from VCF to Genotype."""
-    main(vcf_file, num_processing, output_file)
+    main(vcf_file, output_file)
 
 
 if __name__ == '__main__':
--- pybioinformatic/vcf.py
+++ pybioinformatic/vcf.py
@@ -6,62 +6,22 @@ Author: xuwenlin
 E-mail: wenlinxu.njfu@outlook.com
 """
 from typing import Union
-from io import TextIOWrapper, StringIO
+from io import TextIOWrapper
 from os.path import abspath
 from gzip import GzipFile
-from natsort import natsort_key
-from pandas import read_table, Series, DataFrame, concat
-from pybioinformatic import TaskManager
-
-
-def __record_to_genotype(row: Series):
-    """
-    Convert record to GenoType.
-    The Series values are REF, ALT, and record respectively.
-    """
-    ref, alt, record = row[0], row[1], row[2]
-    gt = record.split(':')[0]
-    if gt == '0/0':
-        allele = ref * 2
-    elif gt == '0/1':
-        allele = ref + alt
-    elif gt == '1/1':
-        allele = alt * 2
-    else:
-        allele = 'NA'
-    return allele
-
-
-def vcf_to_genotype(df: DataFrame) -> DataFrame:
-    """
-    Convert VCF to GenoType.
-    The top 5 columns of DataFrame must be #CHROM, POS, ID, REF, and ALT.
-    The header of DataFrame is not None.
-    """
-    new_col = ['Chrom', 'Position', 'Ref', 'Alt']
-    for sample in df.columns[5:]:
-        new_col.append(sample)
-        df[sample] = df.loc[:, ['REF', 'ALT', sample]].apply(__record_to_genotype, axis=1)
-    df['ID'] = df['#CHROM'] + '_' + df['POS']
-    df.set_index('ID', drop=True, inplace=True)
-    df.columns = new_col
-    df.drop(columns='Alt', inplace=True)
-    return df
 
 
 class VCF:
     def __init__(self, path: Union[str, TextIOWrapper]):
-        self.name = abspath(path) if isinstance(path, str) else \
-            abspath(path.name.replace('<', '').replace('>', ''))
+        self.name = abspath(path) if isinstance(path, str) else abspath(path.name)
         if isinstance(path, str):
-            self.__open = open(path)
+            if path.endswith('gz'):
+                self.__open = GzipFile(path)
+            else:
+                self.__open = open(path)
         else:
-            if path.name == '<stdin>':
-                string_io = StringIO()
-                for line in path:
-                    string_io.write(line)
-                string_io.seek(0)
-                self.__open = string_io
+            if path.name.endswith('gz'):
+                self.__open = GzipFile(path.name)
             else:
                 self.__open = path
 
@@ -74,40 +34,33 @@ class VCF:
         except AttributeError:
             pass
 
-    @staticmethod
-    def __use_col(column_name: str):
-        except_col = {'QUAL', 'FILTER', 'INFO', 'FORMAT'}
-        return True if column_name not in except_col else False
-
-    def to_dataframe(self):
-        if 'gz' in self.name:  # read xxx.vcf.gz file
-            df = read_table(self.name, usecols=self.__use_col, dtype=str,
-                            skiprows=sum(1 for line in GzipFile(self.name)
-                                         if str(line, 'utf8').startswith('##')))
-        else:
-            skip_rows = sum(1 for line in self.__open if line.startswith('##'))
-            self.__open.seek(0)
-            df = read_table(self.__open, usecols=self.__use_col, dtype=str, skiprows=skip_rows)
-        return df
-
-    def to_dataframes(self, chunk_size: int = 10000):
-        if 'gz' in self.name:  # read xxx.vcf.gz file
-            dfs = read_table(self.name, usecols=self.__use_col, dtype=str, chunksize=chunk_size,
-                             skiprows=sum(1 for line in GzipFile(self.name)
-                                          if str(line, 'utf8').startswith('##')))
-        else:
-            skip_rows = sum(1 for line in self.__open if line.startswith('##'))
-            self.__open.seek(0)
-            dfs = read_table(self.__open, usecols=self.__use_col, dtype=str, chunksize=chunk_size,
-                             skiprows=skip_rows)
-        return dfs
-
-    def to_genotype(self, num_processing: int) -> DataFrame:
+# Format conversion method==============================================================================================
+    def to_genotype(self):
         """Convert VCF to GenoType."""
-        params = [(df,) for df in self.to_dataframes()]
-        tkm = TaskManager(processing_num=num_processing, params=params)
-        results = tkm.parallel_run_func(vcf_to_genotype)
-        results = [i.get() for i in results]
-        gt = concat(results)
-        gt.sort_index(key=natsort_key, inplace=True)
-        return gt
+        for line in self.__open:
+            if not isinstance(line, str):
+                line = str(line, 'utf8')
+            if not line.startswith('#') and line.strip():
+                split = line.strip().split('\t')
+                chr_name, position, ID, ref, alt, = split[:5]
+                ID = f'{chr_name}_{position}'
+                samples = split[9:]
+                samples_gt = []
+                for sample in samples:
+                    gt = sample.split(':')[0]
+                    if gt == './.':
+                        gt = 'NA'
+                    elif gt == '0/0':
+                        gt = ref * 2
+                    elif gt == '0/1':
+                        gt = ''.join(sorted(ref + alt))
+                    elif gt == '1/1':
+                        gt = alt * 2
+                    samples_gt.append(gt)
+                samples_gt = '\t'.join(samples_gt)
+                yield f'{ID}\t{chr_name}\t{position}\t{ref}\t{samples_gt}'
+            elif not line.startswith('##') and line.startswith('#'):
+                split = line.strip().split('\t')
+                header = 'ID\tChrom\tPosition\tRef\t'
+                samples = '\t'.join(split[9:])
+                yield header + samples
