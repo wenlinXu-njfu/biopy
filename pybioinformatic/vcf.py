382db19d38386026fbf646c8afc461dc835641d3
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2024-01-20 22:45:37 +0800

    update vcf2gt

--- pybioinformatic/vcf.py
+++ pybioinformatic/vcf.py
@@ -44,6 +44,7 @@ class VCF:
                 split = line.strip().split('\t')
                 chr_name, position, ID, ref, alts, = split[:5]
                 alts = alts.split(',')
+                alts.insert(0, ref)
                 ID = f'{chr_name}_{position}'
                 samples = split[9:]
                 samples_gt = []
@@ -52,14 +53,29 @@ class VCF:
                     if gt == './.':
                         gt = 'NA'
                     else:
-                        alts.insert(0, ref)
                         alleles_index = gt.replace('|', '/').split('/')
                         # SNP
                         if len(alts[int(alleles_index[0])]) == len(alts[int(alleles_index[1])]) == 1:
                             gt = alts[int(alleles_index[0])] + alts[int(alleles_index[1])]
                         # INDEL
                         else:
-                            gt = alts[int(alleles_index[0])] + '/' + alts[int(alleles_index[1])]
+                            if len(ref) == len(alts[int(alleles_index[0])]) == 1:
+                                alt = alts[int(alleles_index[1])][1:]
+                                gt = f'-/ins{alt}'
+                            elif len(ref) == len(alts[int(alleles_index[0])]) > 1:
+                                alt = alts[int(alleles_index[0])][1:]
+                                gt = f'-/del{alt}'
+                            elif ref == alts[int(alleles_index[0])] == alts[int(alleles_index[1])]:
+                                gt = '-'
+                            else:
+                                alt1 = f'del{ref.replace(alts[int(alleles_index[0])], "", 1)}' \
+                                    if len(ref) > len(alts[int(alleles_index[0])]) else \
+                                    f'ins{alts[int(alleles_index[0])].replace(ref, "", 1)}'
+                                alt2 = f'del{ref.replace(alts[int(alleles_index[1])], "", 1)}' \
+                                    if len(ref) > len(alts[int(alleles_index[1])]) else \
+                                    f'ins{alts[int(alleles_index[1])].replace(ref, "", 1)}'
+                                gt = f'{alt1}/{alt2}'
+                            # gt = alts[int(alleles_index[0])] + '/' + alts[int(alleles_index[1])]
                     samples_gt.append(gt)
                 samples_gt = '\t'.join(samples_gt)
                 yield f'{ID}\t{chr_name}\t{position}\t{ref}\t{samples_gt}'
