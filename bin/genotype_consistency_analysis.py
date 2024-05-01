128721a127b68a2443b6aa8489c224d90ea9a048
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2024-05-01 16:00:21 +0800

    add font_name option

--- bin/genotype_consistency_analysis.py
+++ bin/genotype_consistency_analysis.py
@@ -11,16 +11,18 @@ from io import TextIOWrapper
 from os import makedirs, getcwd
 import click
 from pybioinformatic import GenoType, Timer, Displayer
-displayer = Displayer(__file__.split('/')[-1], version='0.1.0')
+displayer = Displayer(__file__.split('/')[-1], version='0.1.1')
 
 
 def main(gt_file1: Union[str, TextIOWrapper],
          gt_file2: Union[str, TextIOWrapper],
          database_compare: bool,
+         font_name: str,
          output_path: str):
     makedirs(output_path, exist_ok=True)
     with GenoType(gt_file1) as gt1, GenoType(gt_file2) as gt2:
-        gt1.compare(gt2, output_path=output_path) if database_compare else \
+        gt1.compare(gt2, output_path=output_path, font_name=font_name) \
+            if database_compare else \
             gt1.self_compare(gt2, output_path=output_path)
 
 
@@ -35,15 +37,18 @@ def main(gt_file1: Union[str, TextIOWrapper],
               default=True, show_default=True,
               help='Specify comparison mode. Database compare means test sample compare with database sample. '
                    'Self compare means each sample compares with its different test batch.')
+@click.option('-f', '--font_name', 'font_name',
+              metavar='<str>', default='Arial', show_default=True,
+              help='Set figure font, if "--database-compare" specified.')
 @click.option('-o', '--output-path', 'output_path',
               metavar='<path>', default=getcwd(), show_default=True,
               help='Output file path, if not exist, automatically created.')
 @click.option('-V', '--version', 'version', help='Show author and version information.',
               is_flag=True, is_eager=True, expose_value=False, callback=displayer.version_info)
 @Timer('Genotype consistency analysis underway.')
-def run(database_gt, test_gt, database_compare, output_path):
+def run(database_gt, test_gt, database_compare, font_name, output_path):
     """Genotype consistency analysis."""
-    main(database_gt, test_gt, database_compare, output_path)
+    main(database_gt, test_gt, database_compare, font_name, output_path)
 
 
 if __name__ == '__main__':
