6ff6b4e970b5ae43cbf0ff9bd18696e6b766cb11
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2024-05-11 21:54:45 +0800

    update option

--- plot_lib/plot_venn.py
+++ plot_lib/plot_venn.py
@@ -1,50 +1,60 @@
 #!/usr/bin/env python
 """
 File: plot_venn.py
-Description: Draw the venn plot
+Description: Draw the venn plot.
 CreateDate: 2022/2/22
 Author: xuwenlin
 E-mail: wenlinxu.njfu@outlook.com
 """
-from os import listdir
+from io import TextIOWrapper
+from typing import Tuple
 from venn import venn
 import matplotlib.pyplot as plt
 import click
 from pybioinformatic import Displayer
-displayer = Displayer(__file__.split('/')[-1], version='0.1.0')
+displayer = Displayer(__file__.split('/')[-1], version='0.2.0')
 
 
-def main(in_dir: str, figure_size: tuple, out_prefix: str, fmt: str):
+def main(input_files: Tuple[TextIOWrapper],
+         group_name: Tuple[str],
+         figure_size: Tuple[float, float],
+         output_file: str):
     plt.rcParams['pdf.fonttype'] = 42
     plt.rcParams['font.family'] = 'Arial'
-    files = listdir(in_dir)
     data_set_dict = {}
-    for file in files:
-        data_set_dict[file] = set(line.strip() for line in open(f'{in_dir}/{file}') if line.strip())
+    for file, name in zip(input_files, group_name):
+        data_set_dict[name] = set(line.strip() for line in file if line.strip())
     venn(data_set_dict, legend_loc=(0.8, 0.8), figsize=figure_size)
-    plt.savefig(f'{out_prefix}.{fmt}')
+    plt.savefig(output_file, bbox_inches='tight')
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('-i', '--input_dir', 'input_dir',
-              metavar='<dir>', required=True,
-              help='Input directory where data files are stored.')
-@click.option('-s', '--figure_size',
-              metavar='<str>', default='8.0x8.0', show_default=True,
+@click.argument('input_files', metavar='<files|stdin>', type=click.File('r'), nargs=-1, required=True)
+@click.option('-g', '--group-name', 'group_name',
+              metavar='<str>', required=True,
+              help='Separated by commas group name of different sets. '
+                   'The number of names must be the same as the number of input files.')
+@click.option('-f', '--figure-size', 'figure_size',
+              metavar='<str>', default='6.4x4.8', show_default=True,
               help='Figure size.')
-@click.option('-o', '--output_prefix', 'output_prefix',
-              metavar='<str>', default='venn', show_default=True,
-              help='Prefix of output file.')
-@click.option('-O', '--output_format', 'output_format',
-              type=click.Choice(['eps', 'jpeg', 'jpg', 'pdf', 'pgf', 'png', 'ps', 'raw', 'rgba', 'svg', 'svgz', 'tif', 'tiff']),
-              metavar='<str>', default='pdf', show_default=True,
-              help='The format of output file (support eps, jpeg, jpg, pdf, pgf, png, ps, raw, rgba, svg, svgz, tif, and tiff).')
+@click.option('-o', '--output-file', 'output_file',
+              metavar='<file>', default='venn.pdf', show_default=True,
+              help='Output file (support eps, jpeg, jpg, pdf, pgf, png, ps, raw, rgba, svg, svgz, tif, and tiff).')
 @click.option('-V', '--version', 'version', help='Show author and version information.',
               is_flag=True, is_eager=True, expose_value=False, callback=displayer.version_info)
-def run(input_dir, figure_size, out, output_format):
+def run(input_files, group_name, figure_size, output_file):
     """Draw the venn plot."""
     figure_size = tuple(float(i) for i in figure_size.split('x'))
-    main(input_dir, figure_size, out, output_format)
+    group_names = tuple(group_name.split(','))
+    if len(figure_size) != 2:
+        click.echo('\033[31mError: Invalid value of "-s, --figure_size" option.\033[0m', err=True)
+    elif len(input_files) != len(group_names):
+        click.echo('\033[31mError: The number of input files is inconsistent with the number of collection names.\033[0m', err=True)
+    else:
+        main(input_files=input_files,
+             group_name=group_names,
+             figure_size=figure_size,
+             output_file=output_file)
 
 
 if __name__ == '__main__':
