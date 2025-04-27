9d24a67b4fed354460b34962ae3b3c6c4ef4f74e
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2025-04-27 12:02:56 +0800

    update

--- file_format_conversion_lib/format_fasta.py
+++ file_format_conversion_lib/format_fasta.py
@@ -12,7 +12,7 @@ from re import findall
 from natsort import natsort_key
 import click
 from pybioinformatic import Fasta, Displayer
-displayer = Displayer(__file__.split('/')[-1], version='0.1.0')
+displayer = Displayer(__file__.split('/')[-1], version='0.2.0')
 
 
 def main(fasta_file: Union[str, TextIOWrapper],
@@ -20,47 +20,71 @@ def main(fasta_file: Union[str, TextIOWrapper],
          char_num: int = 60,
          sort_by_len: bool = True,
          sort_by_id: bool = True,
+         reverse_id: bool = False,
+         reverse_len: bool = False,
          out_file: TextIOWrapper = None):
     with Fasta(fasta_file) as fa:
         seq_obj_generator = fa.merge_sequence(parse_seqids) if char_num == 0 else \
             fa.split_sequence(parse_seqids, char_num)
         seq_obj_list = [seq_obj for seq_obj in seq_obj_generator]
         if sort_by_len and not sort_by_id:
-            seq_obj_list.sort(key=lambda i: i.len)
+            seq_obj_list.sort(key=lambda i: i.len, reverse=reverse_len)
         elif not sort_by_len and sort_by_id:
-            seq_obj_list.sort(key=lambda seq_obj: natsort_key(seq_obj.id))
+            seq_obj_list.sort(key=lambda seq_obj: natsort_key(seq_obj.id), reverse=reverse_id)
         elif sort_by_len and sort_by_id:
-            seq_obj_list.sort(key=lambda i: (i.len, findall(r'[a-zA-Z]+', i.id)[0], int(findall(r'\d+', i.id)[0])))
+            if reverse_len:
+                seq_obj_list.sort(
+                    key=lambda seq_obj: (-seq_obj.len, natsort_key(seq_obj.id), int(findall(r'\d+', seq_obj.id)[0])),
+                    reverse=reverse_id
+                )
+            else:
+                seq_obj_list.sort(
+                    key=lambda seq_obj: (seq_obj.len, natsort_key(seq_obj.id), int(findall(r'\d+', seq_obj.id)[0])),
+                    reverse=reverse_id
+                )
         for seq_obj in seq_obj_list:
             click.echo(seq_obj, out_file)
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('-i', '--fasta_file', 'fasta_file',
-              metavar='<fasta file|stdin>', type=click.File('r'), required=True,
-              help='Input FASTA file.')
+@click.argument('fasta_file', metavar='<fasta file|stdin>', nargs=1, required=True, type=click.File('r'))
 @click.option('-p', '--parse_seqids', 'parse_seqids',
               is_flag=True, flag_value=True,
               help='Parse sequence id.')
 @click.option('-n', '--char_num', 'char_num',
               metavar='<int>', type=int, default=60, show_default=True,
               help='Specify how many character show in per line, 0 presents one line show per sequence.')
-@click.option('-L', '--sort_by_len', 'sort_by_len',
+@click.option('-l', '--sort_by_len', 'sort_by_len',
               is_flag=True, flag_value=True,
               help='Sort sequence by length. If both "-L, --sort_by_len" and "-I, --sort_by_id" options are specified, '
                    'sort by length first then id by default.')
-@click.option('-I', '--sort_by_id', 'sort_by_id',
+@click.option('-i', '--sort_by_id', 'sort_by_id',
               is_flag=True, flag_value=True,
               help='Sort sequence by id. If both "-L, --sort_by_len" and "-I, --sort_by_id" options are specified, '
                    'sort by length first then id by default.')
+@click.option('-r', '--reverse-id', 'reverse_id',
+              is_flag=True, flag_value=True,
+              help='Reverse the result of sort by id.')
+@click.option('-R', '--reverse-len', 'reverse_len',
+              is_flag=True, flag_value=True,
+              help='Reverse the result of sort by length.')
 @click.option('-o', '--output_file', 'outfile',
               metavar='<fasta file|stdout>', type=click.File('w'),
               help='Output file, stdout by default.')
 @click.option('-V', '--version', 'version', help='Show author and version information.',
               is_flag=True, is_eager=True, expose_value=False, callback=displayer.version_info)
-def run(fasta_file, parse_seqids, char_num, sort_by_len, sort_by_id, outfile):
+def run(fasta_file, parse_seqids, char_num, sort_by_len, sort_by_id, reverse_id, reverse_len, outfile):
     """Make each sequence to be displayed in a single line or in multiple lines and sort sequence by length or ID."""
-    main(fasta_file, parse_seqids, char_num, sort_by_len, sort_by_id, outfile)
+    main(
+        fasta_file=fasta_file,
+        parse_seqids=parse_seqids,
+        char_num=char_num,
+        sort_by_len=sort_by_len,
+        sort_by_id=sort_by_id,
+        reverse_id=reverse_id,
+        reverse_len=reverse_len,
+        out_file=outfile
+    )
 
 
 if __name__ == '__main__':
