e88a59898aec1788166b256c9b8db57e78e66a24
wenlinXu-njfu <wenlinxu.njfu@outlook.com>
2025-05-06 10:43:49 +0800

    pybioinformatic==1.0.6

--- configure.py
+++ configure.py
@@ -3,7 +3,7 @@ from os.path import abspath, isfile
 from shutil import which
 
 path = '/'.join(abspath(__file__).split('/')[:-1])
-system(command="pip install pybioinformatic==1.0.5 biopython fire pyyaml requests schema scipy venn")
+system(command="pip install pybioinformatic==1.0.6 biopython fire pyyaml requests schema scipy venn")
 system(command=f"sed -i 's/\r//' {path}/bin/*")
 python_path = which('python')
 system(command=f"sed -i 's@#!/usr/bin/env python@#!{python_path}@' {path}/bin/*")
